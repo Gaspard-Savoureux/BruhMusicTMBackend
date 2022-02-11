09ad123fec8c82b28536750f78cf17f1eada6ade
Albert Robillard <samuel.a.lajoie@gmail.com>
2022-02-11 09:00:07 -0500

    quick fix music

--- router/music/index.js
+++ router/music/index.js
@@ -61,8 +61,8 @@ router.get('/', async (req, res) => {
   const { title } = req.query;
   const searchRelatedExist = await db('music').where('title', 'like', `%${title}%`);
 
-  console.log(searchRelatedExist.length());
-  if (searchRelatedExist.length() === 0) return res.status(404).send({ message: 'Aucun résultats retourner pour cette recherche' });
+  const length = await searchRelatedExist.length();
+  if (length === 0) return res.status(404).send({ message: 'Aucun résultats retourner pour cette recherche' });
 
   return res.status(201).send(searchRelatedExist);
 });
