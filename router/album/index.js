088ba36e92fdd74ff9cb68605128883b1b64a965
Albert Robillard <samuel.a.lajoie@gmail.com>
2022-02-11 10:18:24 -0500

    quick fix music

--- router/album/index.js
+++ router/album/index.js
@@ -34,7 +34,8 @@ router.post('/', authMiddleware, async (req, res) => {
   const length = musicIds.length - 1;
 
   musicIds.forEach((id) => {
-    whereId += id == 0 ? `WHERE id = ${musicIds[0]}` : `OR id = ${musicIds[id]}`;
+    console.log(id);
+    whereId += id === 0 ? `WHERE id = ${musicIds[0]}` : `OR id = ${musicIds[id]}`;
   });
 
   console.log(whereId);
