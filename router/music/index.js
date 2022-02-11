e26688258fa8eb7be6b77325f91c3f7f8c7da28b
Albert Robillard <samuel.a.lajoie@gmail.com>
2022-02-11 09:04:30 -0500

    quick fix music

--- router/music/index.js
+++ router/music/index.js
@@ -63,7 +63,7 @@ router.get('/', async (req, res) => {
 
   console.log(searchRelatedExist);
 
-  if (!searchRelatedExist) return res.status(404).send({ message: 'Aucun résultats retourner pour cette recherche' });
+  if (!searchRelatedExist[0]) return res.status(404).send({ message: 'Aucun résultats retourner pour cette recherche' });
 
   return res.status(201).send(searchRelatedExist);
 });
